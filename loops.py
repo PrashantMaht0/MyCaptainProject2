051361097cc83d6d3f78411841c9e78e12b38e94
PrashantMaht0 <119067260+PrashantMaht0@users.noreply.github.com>
2023-01-29 12:57:13 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+#program to print positive number in range
+mylist1=[12, -7, 5, 64, -14]
+mylist2=[12, 14, -95, 3]
+for number in mylist1:
+    if number > 0:
+        print("positve numbers in list 1 is:",number)
+
+print("\n")
+
+for number in mylist2:
+    if number > 0:
+        print("positve numbers in list 2 is:",number)
